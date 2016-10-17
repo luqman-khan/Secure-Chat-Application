ed26f938e2817430b309e3db2d05aefee0f87b42
Luqman Khan <lukku.khan2050@gmail.com>
2016-10-16 17:55:49 -0700

    added verification url

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,3 +1,7 @@
 class WelcomesController < ApplicationController
   protect_from_forgery with: :exception
+  def letsencrypt
+    # use your code here, not mine
+    render text: "K3qow1aVCBhOtkVuIO0dc1JMJFmUEMmf1jtXJCKKTlQ.09snjbs1AwJYZya5OYHMmHUv8KQhVNQPmL9S5CYw5iU"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcomes#welcome_page"
+  get '/.well-known/acme-challenge/:id' => 'welcomes#letsencrypt'
 end
