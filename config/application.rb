ff9c14dfe6b20c41cf8e3e4224f3188bbb1f08fc
Luqman Khan <lukku.khan2050@gmail.com>
2016-10-17 03:35:07 -0700

    adding force ssl

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,9 @@ Bundler.require(*Rails.groups)
 
 module ChatApp
   class Application < Rails::Application
-  	config.force_ssl = false
+  	production do
+  		config.force_ssl = true
+  	end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
