63217fb36bad6743175a1fc843a4c85427731381
Luqman Khan <lukku.khan2050@gmail.com>
2016-10-17 03:45:42 -0700

    force ssl correction

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,6 @@ Bundler.require(*Rails.groups)
 
 module ChatApp
   class Application < Rails::Application
-  	production do
-  		config.force_ssl = true
-  	end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
