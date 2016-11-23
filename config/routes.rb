9d056ada8d801898327752ee68f1ca6a11a3facc
Luqman Khan <lukku.khan2050@gmail.com>
2016-11-23 10:40:11 -0800

    temp route change

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-	devise_for :users, controllers: { registrations: 'users/registrations' }
+	# devise_for :users, controllers: { registrations: 'users/registrations' }
 
-	devise_scope :user do
-		root "devise/sessions#new"
-	end
-	resource :welcomes do
-		get 'welcome_page'
-	end
+	# devise_scope :user do
+	# 	root "devise/sessions#new"
+	# end
+	# resource :welcomes do
+	# 	get 'welcome_page'
+	# end
+	root 'welcomes#welcome_page'
   # get '/.well-known/acme-challenge/:id' => 'welcomes#letsencrypt'
 end
