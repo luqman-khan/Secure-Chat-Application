93bafe6c3a68fc3275c9721f954db41082ceec0d
Luqman Khan <lukku.khan2050@gmail.com>
2016-12-06 15:17:20 -0800

    back to env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
    :address              => "smtp.gmail.com",
    :port                 => 587,
    :domain               => 'mad-hats.me:80',
-   :user_name            => "lukku.khan2050@gmail.com",
-   :password             => "domazait",
+   :user_name            => ENV['GMAIL_USERNAME'],
+   :password             => ENV['GMAIL_PASSWORD'],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
