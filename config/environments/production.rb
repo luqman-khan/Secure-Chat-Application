86d6f5f9bb97bdd749914bface96b71519187b7b
Luqman Khan <lukku.khan2050@gmail.com>
2016-12-06 14:24:55 -0800

    mail for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,13 +27,12 @@ Rails.application.configure do
   config.assets.compile = false
 
   config.action_mailer.perform_caching = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {:host => 'mad-hats.me'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
+   :domain               => 'mad-hats.me:80'
    :user_name            => ENV['GMAIL_USERNAME'],
    :password             => ENV['GMAIL_PASSWORD'],
    :authentication       => "plain",
